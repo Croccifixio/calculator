faf546094aa33e0b0a085199fe85ea033698dcac
Croccifixio <aizenkrosis@hotmail.com>
2020-03-07 20:24:01 +0100

    Style Keypad

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,20 @@
 #app
   History
   Calculator
+  Keypad
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
 import Calculator from './components/Calculator.vue'
 import History from './components/History.vue'
+import Keypad from './components/Keypad.vue'
 
 @Component({
   components: {
     Calculator,
     History,
+    Keypad,
   },
 })
 export default class App extends Vue {}
@@ -25,4 +28,6 @@ export default class App extends Vue {}
   color #2c3e50
   margin 60px auto 0
   max-width 600px
+  padding-left 15px
+  padding-right 15px
 </style>
---
+++ src/components/Keypad.vue
@@ -0,0 +1,65 @@
+<template lang="pug">
+.keypad
+  button.one sin
+  button.one cos
+  button.one tan
+  button.one (
+  button.one )
+  button.one 7
+  button.one 8
+  button.one 9
+  button.one *
+  button.one /
+  button.one 4
+  button.one 5
+  button.one 6
+  button.one +
+  button.one -
+  button.one 1
+  button.one 2
+  button.one 3
+  button.one del
+  button.two =
+  button.one AC
+  button.one C
+  button.one 0
+  button.one .
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component
+export default class Keypad extends Vue {}
+</script>
+
+<style lang="stylus" scoped>
+.keypad
+  --button-size 8ch
+  background-color #000
+  border 2px solid #000
+  display inline-grid
+  grid-gap 2px
+  grid-template-columns repeat(5, 1fr)
+  grid-template-rows repeat(5, 1fr)
+
+button
+  align-items center
+  background-color #fff
+  border 0
+  box-shadow none
+  display flex
+  justify-content center
+  outline none
+  padding 0
+
+.one
+  height var(--button-size)
+  width var(--button-size)
+
+.two
+  grid-column-start 5
+  grid-row-start 4
+  grid-row-end 6
+  width var(--button-size)
+</style>
